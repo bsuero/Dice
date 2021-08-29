291b2f0c37aa3f53dd88b9a0846b3a78b3785b46
Brany Suero <branysuero@gmail.com>
2021-08-29 19:28:56 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-//If player 1 wins
+//If player 1 wins test
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
